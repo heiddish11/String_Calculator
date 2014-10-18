2c237c66ffef60fd6047018e13f0fc7f660baa9b
heiddis <heiddish11@ru.is>
2014-10-18 18:56:54 +0000

    sett inn kodi fyrir 1 og 2 tolur

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,7 +3,21 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		if(text.equals("")){
-			return 0;
-		}
+                if(text.equals("")){
+                        return 0;
+                }
+                else if(text.contains(",")){
+                        return sum(splitNumbers(text));
+                }
+                else
+                        return 1;
+        }
+
+        private static int toInt(String number){
+                return Integer.parseInt(number);
+        }
+
+        private static String[] splitNumbers(String numbers){
+            return numbers.split(",");
+        }
 }
